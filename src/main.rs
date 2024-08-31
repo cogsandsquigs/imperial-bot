e4356568523b91bb8a166b7cfc1a5fd5a4fd379d
Ian Pratt <ianjdpratt@gmail.com>
2024-08-31 14:10:18 -0500

    Add logging of panics + such

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use log::info;
 #[tokio::main]
 async fn main() {
     dotenv().ok(); // Load environment variables from .env files.
-
-    Builder::from_env(Env::default().filter("LOG_LEVEL")).init();
+    Builder::from_env(Env::default().filter("LOG_LEVEL")).init(); // Initialize logger.
+    log_panics::init(); // Log panics instead of printing them
 
     info!("Starting up...");
 
